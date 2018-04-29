af65b9675ab39f537d99959b9497a35b62fdda08
will scott <wtscott@gmail.com>
2018-04-29 00:18:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,14 @@
+
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
 
 $(document).ready(function(){
 
 // call functions here
+  getIt();
 
 });
