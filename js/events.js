69f6dc2c170510cc24e35e050e03f45d5f8c36ea
will scott <wtscott@gmail.com>
2018-04-29 00:23:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 
 function submitIt(){
   $('form').on('submit', function() {
-    alert('Your form is going to be submitted now.");
+    alert('Your form is going to be submitted now.');
   });
 }
 
